367eb2a22929a1b105a2838f377147cc375f1f3c
Alexandre Holzhey <alexandre.holzhey@sonarsource.com>
2020-09-23 15:36:16 +0200

    add parsing and output rendering

--- prtree.js
+++ prtree.js
@@ -1,25 +1,76 @@
 const { Octokit } = require("@octokit/rest");
-const octokit = new Octokit({
-  auth: "fa2d96785bcfe7ccf85cdd5cedae4f6354ba4b51"
-});
+const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
+const colors = require("colors")
 
-octokit.paginate(
-  "GET /repos/:owner/:repo/pulls?state=:state",
-  {
-    owner: "SonarSource",
-    repo: "sonarcloud-core",
-    state: "open"
+octokit.repos.getBranch({
+  owner: "SonarSource",
+  repo: "sonarcloud-core",
+  branch: "master"
+}).then(branch => {
+  var masterNode = {
+    headSha: branch.data.commit.sha,
+    ref: "master",
+    output: ("Master at #" + branch.data.commit.sha.substr(0,7)).green,
+    children: []
+  }
+
+  octokit.paginate(
+    "GET /repos/:owner/:repo/pulls?state=:state",
+    {
+      owner: "SonarSource",
+      repo: "sonarcloud-core",
+      state: "open"
+    })
+    .then((data) => {
+      data.sort((a, b) => a.id - b.id).forEach(pr => {
+        var newNode = {
+          headSha: pr.head.sha,
+          baseSha: pr.base.sha,
+          baseRef: pr.base.ref,
+          ref: pr.head.ref,
+          output: renderOutput(pr),
+          children: []
+        }
+
+        addNode(masterNode, newNode)
+      });
+
+      outputAsTree(masterNode)
+    })
+})
+
+function addNode(node, newNode) {
+  if (node.ref == newNode.baseRef) {
+    if (newNode.baseSha != node.headSha) {
+      newNode.output += " " + "(branch have new commits)".bgRed.white
+    }
+    node.children.push(newNode)
+  } else {
+    node.children.forEach((childNode, index) => {
+      addNode(childNode, newNode)
+    })
+  }
+}
+
+function renderOutput(pr) {
+  var output = "PR ".green
+  if (pr.draft) {
+    output += "(DRAFT) ".yellow
+  }
+  output += (pr.number + " by " + pr.user.login + ": \n").green;
+  output += pr.head.ref + " at #" + pr.head.sha.substr(0,7) + " ---> " + pr.base.ref + " at #" + pr.base.sha.substr(0,7)
+
+  return output
+}
+
+function outputAsTree(node, level = 0) {
+  node.output.split("\n").forEach(line => {
+    console.log("  ".repeat(level) + line)
   })
-  .then((data) => {
-    data.forEach(pr => {
-      var output = "PR "
-      if (pr.draft) {
-        output += "(draft) "
-      }
-      output += pr.number + " by " + pr.user.login + ": ";
-      output += pr.head.ref + " ==> " + pr.base.ref
-      console.log(output)
-      // console.log(pr.head.sha, pr.base.sha)
-    });
-  });
-
+  if (node.children.length > 0) {
+    console.log("  ".repeat(level) + "\\")
+  }
+  node.children.forEach(childNode => {
+    outputAsTree(childNode, level + 1)
+  })
+}
